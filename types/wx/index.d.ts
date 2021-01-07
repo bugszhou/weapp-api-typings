57d63d3aeeba9225d6f410a0c2551df56ee54cb0
youzhao.zhou <youzhao.zhou@verystar.cn>
2021-01-07 20:19:47 +0800

    feat: 增加全局变量

--- types/wx/index.d.ts
+++ types/wx/index.d.ts
@@ -29,20 +29,32 @@ SOFTWARE.
 /// <reference path="./lib.wx.event.d.ts" />
 
 declare namespace WechatMiniprogram {
-    type IAnyObject = Record<string, any>
-    type Optional<F> = F extends (arg: infer P) => infer R ? (arg?: P) => R : F
-    type OptionalInterface<T> = { [K in keyof T]: Optional<T[K]> }
-    interface AsyncMethodOptionLike {
-        success?: (...args: any[]) => void
-    }
-    type PromisifySuccessResult<
-        P,
-        T extends AsyncMethodOptionLike
-    > = P extends { success: any }
-        ? void
-        : P extends { fail: any }
-        ? void
-        : P extends { complete: any }
-        ? void
-        : Promise<Parameters<Exclude<T['success'], undefined>>[0]>
+  type IAnyObject = Record<string, any>;
+  type Optional<F> = F extends (arg: infer P) => infer R ? (arg?: P) => R : F;
+  type OptionalInterface<T> = { [K in keyof T]: Optional<T[K]> };
+  interface AsyncMethodOptionLike {
+    success?: (...args: any[]) => void;
+  }
+  type PromisifySuccessResult<P, T extends AsyncMethodOptionLike> = P extends {
+    success: any;
+  }
+    ? void
+    : P extends { fail: any }
+    ? void
+    : P extends { complete: any }
+    ? void
+    : Promise<Parameters<Exclude<T["success"], undefined>>[0]>;
 }
+
+/** 引入插件。返回插件通过 `main` 暴露的接口。 */
+declare function requirePlugin(
+  /** 需要引入的插件的 alias */
+  module: string,
+): any;
+
+/** 插件引入当前使用者小程序。返回使用者小程序通过 [插件配置中 `export` 暴露的接口](https://developers.weixin.qq.com/miniprogram/dev/framework/plugin/using.html#%E5%AF%BC%E5%87%BA%E5%88%B0%E6%8F%92%E4%BB%B6)。
+ *
+ * 该接口只在插件中存在
+ *
+ * 最低基础库： `2.11.1` */
+declare function requireMiniProgram(): any;
